25d5d65e7c833c06a7111ebf154e0fd777d14a96
dylon <dmychel@tutanota.com>
2024-06-01 01:53:32 -0400

    created form foundation

---
+++ src/form/Form.tsx
@@ -0,0 +1,65 @@
+export default function Form() {
+  return (
+    <section className="password_form">
+      <form>
+        <label htmlFor="min-length">Min Length</label>
+        <select name="min-length" id="min-length">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+        </select>
+
+        <label htmlFor="min-length">Max Length</label>
+        <select name="max-length" id="max-length">
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+        </select>
+
+        <label htmlFor="special">Special Characters</label>
+        <input type="checkbox" name="special" id="special" checked />
+
+        <label htmlFor="numbers">Numbers</label>
+        <input type="checkbox" name="numbers" id="numbers" checked />
+
+        <input type="submit" value="Generate" />
+      </form>
+    </section>
+  );
+}
